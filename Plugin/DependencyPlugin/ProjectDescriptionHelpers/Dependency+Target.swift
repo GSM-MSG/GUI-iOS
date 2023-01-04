13728fd3bf11c5fa34722b1f407dbfd266afaf9a
baegteun <baegteun@gmail.com>
2023-01-05 08:28:14 +0900

    :tada: :: Initializer

---
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift
@@ -0,0 +1,41 @@
+import ProjectDescription
+
+// swiftlint: disable all
+public extension TargetDependency {
+    struct Features {}
+    struct Services {}
+    struct Core {}
+    struct Shared {}
+}
+
+public extension TargetDependency.Features {
+    static let BaseFeature = TargetDependency.project(
+        target: ModulePaths.Features.BaseFeature.targetName(type: .sources),
+        path: .relativeToFeature(ModulePaths.Features.BaseFeature.targetName(type: .sources))
+    )
+}
+
+public extension TargetDependency.Services {
+    static let BaseService = TargetDependency.project(
+        target: ModulePaths.Services.BaseService.targetName(type: .sources),
+        path: .relativeToService(ModulePaths.Services.BaseService.targetName(type: .sources))
+    )
+}
+
+public extension TargetDependency.Core {
+    static let DesignSystem = TargetDependency.project(
+        target: ModulePaths.Core.DesignSystem.targetName(type: .sources),
+        path: .relativeToCore(ModulePaths.Core.DesignSystem.targetName(type: .sources))
+    )
+}
+
+public extension TargetDependency.Shared {
+    static let UtilityModule = TargetDependency.project(
+        target: ModulePaths.Shared.UtilityModule.targetName(type: .sources),
+        path: .relativeToShared(ModulePaths.Shared.UtilityModule.targetName(type: .sources))
+    )
+    static let GlobalThirdPartyLibrary = TargetDependency.project(
+        target: ModulePaths.Shared.GlobalThirdPartyLibrary.targetName(type: .sources),
+        path: .relativeToShared(ModulePaths.Shared.GlobalThirdPartyLibrary.targetName(type: .sources))
+    )
+}
---
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+// swiftlint: disable all
+public enum ModulePaths {
+    case features(Features)
+    case services(Services)
+    case core(Core)
+    case shared(Shared)
+}
+
+public extension ModulePaths {
+    enum Features: String {
+        case BaseFeature
+
+        func targetName(type: MicroTargetType) -> String {
+            "\(self.rawValue)\(type.rawValue)"
+        }
+    }
+}
+
+public extension ModulePaths {
+    enum Services: String {
+        case BaseService
+
+        func targetName(type: MicroTargetType) -> String {
+            "\(self.rawValue)\(type.rawValue)"
+        }
+    }
+}
+
+public extension ModulePaths {
+    enum Core: String {
+        case DesignSystem
+
+        func targetName(type: MicroTargetType) -> String {
+            "\(self.rawValue)\(type.rawValue)"
+        }
+    }
+}
+
+public extension ModulePaths {
+    enum Shared: String {
+        case UtilityModule
+        case GlobalThirdPartyLibrary
+
+        func targetName(type: MicroTargetType) -> String {
+            "\(self.rawValue)\(type.rawValue)"
+        }
+    }
+}
+
+public enum MicroTargetType: String {
+    case interface = "Interface"
+    case sources = ""
+    case testing = "Testing"
+}

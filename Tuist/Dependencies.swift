f3fedb10de21c956c33e8f2a56b1147063c0a1a4
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2023-01-08 22:16:55 +0900

    :zap: 최신 버전

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -8,7 +8,7 @@ let dependencies = Dependencies(
             .remote(url: "https://github.com/GSM-MSG/Moya.git", requirement: .branch("master")),
             .remote(url: "https://github.com/GSM-MSG/MSGLayout", requirement: .exact("1.0.2")),
             .remote(url: "https://github.com/GSM-MSG/Moordinator", requirement: .exact("1.0.0")),
-            .remote(url: "https://github.com/hackiftekhar/IQKeyboardManager", requirement: .exact("6.3.0")),
+            .remote(url: "https://github.com/hackiftekhar/IQKeyboardManager", requirement: .exact("6.5.0")),
             .remote(url: "https://github.com/devxoul/Then", requirement: .exact("3.0.0"))
         ],
         baseSettings: .settings(

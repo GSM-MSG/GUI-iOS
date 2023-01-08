c8964c883bbd427fa55aee95d9e3a1b21de21750
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2023-01-08 21:55:15 +0900

    :zap: 버전 고정 및 변경

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -6,10 +6,10 @@ let dependencies = Dependencies(
     swiftPackageManager: SwiftPackageManagerDependencies(
         [
             .remote(url: "https://github.com/GSM-MSG/Moya.git", requirement: .branch("master")),
-            .remote(url: "https://github.com/GSM-MSG/MSGLayout", requirement: .upToNextMajor(from: "1.0.2")),
-            .remote(url: "https://github.com/GSM-MSG/Moordinator", requirement: .upToNextMajor(from: "1.0.0")),
-            .remote(url: "https://github.com/hackiftekhar/IQKeyboardManager", requirement: .upToNextMajor(from: "6.3.0")),
-            .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "2.0.0"))
+            .remote(url: "https://github.com/GSM-MSG/MSGLayout", requirement: .exact("1.0.2")),
+            .remote(url: "https://github.com/GSM-MSG/Moordinator", requirement: .exact("1.0.0")),
+            .remote(url: "https://github.com/hackiftekhar/IQKeyboardManager", requirement: .exact("6.3.0")),
+            .remote(url: "https://github.com/devxoul/Then", requirement: .exact("3.0.0"))
         ],
         baseSettings: .settings(
             configurations: [

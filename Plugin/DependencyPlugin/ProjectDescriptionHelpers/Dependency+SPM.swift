1028249db712c7359347e4e4efc9db0791c218f0
minaje0917 <k050918@naver.com>
2023-01-08 22:36:19 +0900

    :zap: Dependency-Injection-library

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -6,6 +6,8 @@ public extension TargetDependency {
 
 public extension TargetDependency.SPM {
     static let CombineMoya = TargetDependency.external(name: "CombineMoya")
+    static let DIM = TargetDependency.external(name: "DIM")
+    static let Miniature = TargetDependency.external(name: "Miniature")
 }
 
 public extension Package {
--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -5,7 +5,9 @@ let dependencies = Dependencies(
     carthage: nil,
     swiftPackageManager: SwiftPackageManagerDependencies(
         [
-            .remote(url: "https://github.com/GSM-MSG/Moya.git", requirement: .branch("master"))
+            .remote(url: "https://github.com/GSM-MSG/Moya.git", requirement: .branch("master")),
+            .remote(url: "https://github.com/GSM-MSG/DIM.git", requirement: .exact("1.0.1")),
+            .remote(url: "https://github.com/GSM-MSG/Miniature.git", requirement: .exact("1.1.0"))
         ],
         baseSettings: .settings(
             configurations: [

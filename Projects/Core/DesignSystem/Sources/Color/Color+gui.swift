eb3b45999196c02f1e121031f84a8d9e53123813
baegteun <baegteun@gmail.com>
2023-01-10 00:37:07 +0900

    :recycle: :: method public화

--- Projects/Core/DesignSystem/Sources/Color/Color+gui.swift
+++ Projects/Core/DesignSystem/Sources/Color/Color+gui.swift
@@ -5,7 +5,7 @@ public extension UIColor {
         case system(System)
     }
 
-    static func gui(_ style: GUIColorSystem) -> UIColor {
+    public static func gui(_ style: GUIColorSystem) -> UIColor {
         switch style {
         case let .system(system):
             return system.color

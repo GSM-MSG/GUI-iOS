428a1180120ed268c30befefd241ac2a83ec25b2
baegteun <baegteun@gmail.com>
2023-01-11 19:25:41 +0900

    :recycle: :: default closure

--- Projects/Shared/CombineExtension/Sources/Publisher/Publisher+sink.swift
+++ Projects/Shared/CombineExtension/Sources/Publisher/Publisher+sink.swift
@@ -3,8 +3,8 @@ import Combine
 public extension Publisher {
     func sink<Object: AnyObject>(
         with object: Object,
-        receiveCompletion: @escaping ((Object, Subscribers.Completion<Failure>) -> Void),
-        receiveValue: @escaping ((Object, Output) -> Void)
+        receiveCompletion: @escaping ((Object, Subscribers.Completion<Failure>) -> Void) = { _, _ in },
+        receiveValue: @escaping ((Object, Output) -> Void) = { _, _ in }
     ) -> AnyCancellable {
         self.sink { [weak object] completion in
             guard let object = object else { return }

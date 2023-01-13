a51c2f3cade41fcd00c032de659d580b66d4b808
minaje0917 <k050918@naver.com>
2023-01-13 22:40:13 +0900

    :fire: remove gray color in system

--- Projects/Core/DesignSystem/Sources/Color/GUIColorSystem+System.swift
+++ Projects/Core/DesignSystem/Sources/Color/GUIColorSystem+System.swift
@@ -6,10 +6,6 @@ public extension UIColor.GUIColorSystem {
         case positive
         case black
         case white
-        case g20
-        case g40
-        case g80
-        case g120
     }
 }
 
@@ -27,19 +23,6 @@ public extension UIColor.GUIColorSystem.System {
 
         case .white:
             return DesignSystemAsset.white.color
-            
-        case .g20:
-            return DesignSystemAsset.g20.color
-            
-        case .g40:
-            return DesignSystemAsset.g40.color
-            
-        case .g80:
-            return DesignSystemAsset.g80.color
-            
-        case .g120:
-            return DesignSystemAsset.g120.color
-            
         }
     }
 }

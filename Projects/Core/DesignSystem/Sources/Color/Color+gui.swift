d9528e2ae4d017579c0fa87c6c5c8a629f6f8246
minaje0917 <k050918@naver.com>
2023-01-13 22:46:27 +0900

    :sparkles: GUIColorSystem add gray enum

--- Projects/Core/DesignSystem/Sources/Color/Color+gui.swift
+++ Projects/Core/DesignSystem/Sources/Color/Color+gui.swift
@@ -3,12 +3,16 @@ import UIKit
 public extension UIColor {
     enum GUIColorSystem {
         case system(System)
+        case gray(Gray)
     }
 
     static func gui(_ style: GUIColorSystem) -> UIColor {
         switch style {
         case let .system(system):
             return system.color
+
+        case let .gray(gray):
+            return gray.color
         }
     }
 }
